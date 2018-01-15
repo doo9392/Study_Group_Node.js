4a4270a2d7a703af5d8208908d77c79878b6e965
doo9392 <hackish@hanmail.net>
2018-01-15 18:21:40 +0900

    ch05
    ch05 웹 서버 만들기

--- ch05/README.md
+++ ch05/README.md
@@ -10,7 +10,11 @@ ch05/
   package.json
   package-lock.json
   ch05_test1.js
+  ch05_test2.js
 ```
 
 ### `ch05_test1.js`
 웹서버 만들어보기.
+
+### `ch05_test2.js`
+웹서버를 민들어서 클라이언트 접속 확인.
--- ch05/ch05_test1.js
+++ ch05/ch05_test1.js
@@ -1,7 +1,15 @@
-var http = require("http");
+// var http = require("http");
+// var server = http.createServer();
+// var port = 3000;
+
+// server.listen(port, function() { // listen() : 서버를 실행시켜 대기시킨다. <-> close() 서버를 종요시킨다.
+//     console.log('웹 서버가 시작되었습니다. : %d', port);
+// });
+
+var host = '192.168.0.80'; // ipconif를 이용하여 자신의 pc ip를 넣을 것.
 var server = http.createServer();
 var port = 3000;
 
-server.listen(port, function() {
+server.listen(port, function() { // listen() : 서버를 실행시켜 대기시킨다. <-> close() 서버를 종요시킨다.
     console.log('웹 서버가 시작되었습니다. : %d', port);
 });

a7c6a72f14cec2816268a15a2daa02b3c772e9ec
김승미 <girlstoryk@gmail.com>
2018-01-29 18:54:03 +0900

    ch06
    - 타이핑 중

--- ch06/databaseExample/app2.js
+++ ch06/databaseExample/app2.js
@@ -91,6 +91,38 @@ router.route('/process/login').post((req, res) => {
   }
 });
 
+router.route('/process/adduser').post((req, res) => {
+    console.log('/process/adduser 호출됨.');
+
+    const paramId = req.body.id || req.query.id;
+    const paramPassword = req.body.password || req.query.password;
+    const paramName = req.body.name || req.query.name;
+
+    console.log('요청 파라미터 : ' + paramId + ', ' + paramPassword + ', ' + paramName);
+
+    if (database) {
+        addUser(database, paramId, paramPassword, paramName, (err, result) => {
+            if (err) throw err;
+
+            if (result && result.insertedCount > 0) {
+                console.dir(result);
+
+                res.writeHead('200', {'Content-Type':'text/html;charset=utf8'});
+                res.write('<h2>사용자 추가 성공</h2>');
+                res.end();
+            } else {
+                res.writeHead('200', {'Content-Type':'text/html;charset=utf8'});
+                res.write('<h2>사용자 추가 실패</h2>');
+                res.end();
+            }
+        });
+    } else {
+        res.writeHead('200', {'Content-Type':'text/html;charset=utf8'});
+        res.write('<h2>데이터베이스 연결 실패</h2>');
+        res.end();
+    }
+});
+
 app.use('/', router);  
 
 
@@ -108,7 +140,6 @@ const authUser = (database, id, password, callbak) => {
         callback(err, null);
         return;
       }
-      
       if (docs.length > 0) {
         console.log('아이디 [%s], 비밀번호 [%s]가 일치하는 사용자 찾음.', id, password);
         callback(null, docs);
@@ -120,10 +151,34 @@ const authUser = (database, id, password, callbak) => {
 };
 
 
+// addUser
+const addUser = (database, id, password, name, callback) => {
+    console.log('addUser 호출됨 : ' + id + ', ' + password + ', ' + name);
+
+    const users = database.collection('users');
+
+    users.insertMany([{"id" : id, "password" : password, "name" : name}], (err, result) => {
+        if (err) {
+            callback(err, null);
+            return;
+        }
+
+        if (result.insertedCount > 0) {
+            console.log("사용자 레코드 추가됨 : " + result.insertedCount);
+        } else {
+            console.log("추가된 레코드가 없음.");
+        }
+
+        callbak(null, result);
+    });
+}
+
+
 // errorHandler
 const errorHandler = expressErrorHandler({
   static: { '404': './public/404.html' },
 });
+
 app.use(expressErrorHandler.httpError(404));
 app.use(errorHandler);
 
